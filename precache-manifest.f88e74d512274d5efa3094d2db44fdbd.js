ca4fe8c8293c8d2c095dd0f9baec97e318805233
Mustapha Lourika <mostaphalourika@gmail.com>
2020-06-24 21:22:56 +0100

    Updates

---
+++ precache-manifest.f88e74d512274d5efa3094d2db44fdbd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed73c2e06e2a129d80140362964217cb",
+    "url": "/Corona-Tracker-Maroc/index.html"
+  },
+  {
+    "revision": "75e82c9cc92bc7f88d28",
+    "url": "/Corona-Tracker-Maroc/static/css/main.cda769c3.chunk.css"
+  },
+  {
+    "revision": "b0196dcb0335399b42e5",
+    "url": "/Corona-Tracker-Maroc/static/js/2.62d0e68d.chunk.js"
+  },
+  {
+    "revision": "af5111307236c4c69d6adba51f98588a",
+    "url": "/Corona-Tracker-Maroc/static/js/2.62d0e68d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75e82c9cc92bc7f88d28",
+    "url": "/Corona-Tracker-Maroc/static/js/main.96d40c6a.chunk.js"
+  },
+  {
+    "revision": "05141f370a92ba51c69d",
+    "url": "/Corona-Tracker-Maroc/static/js/runtime-main.e2629759.js"
+  },
+  {
+    "revision": "184b1da21b3788ca7862d32de334c82c",
+    "url": "/Corona-Tracker-Maroc/static/media/corona.184b1da2.jpg"
+  }
+]);
